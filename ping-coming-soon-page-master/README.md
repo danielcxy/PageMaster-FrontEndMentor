28abbbef72f71ad73bdf616583818abf9d4b6721
Daniel Renan <120720081+danielcxy@users.noreply.github.com>
2023-07-05 22:06:40 -0300

    Add files via upload

--- ping-coming-soon-page-master/README.md
+++ ping-coming-soon-page-master/README.md
@@ -1,4 +1,6 @@
 Olá, pessoal! Mais um desafio proposto pelo site FrontEnd Mentor, criação de formulário tipo e-mail, com validação em JavaScript. Falta terminar a parte mobile, aprimorar o código CSS e JavaScript.
+Trabalho concluído em 05/07!
+Qualquer sugestão de código é bem-vinda!
 # Frontend Mentor - Ping coming soon page
 
 ![Design preview for the Ping coming soon page coding challenge](./design/desktop-preview.jpg)
--- ping-coming-soon-page-master/scripts.js
+++ ping-coming-soon-page-master/scripts.js
@@ -3,17 +3,25 @@ const form = document.querySelector('form')
 const email = document.querySelector('input')
 //-------------------------------------- FIM DA DECLARAÇÃO DA DOM ----------------------------------------------//
 
-//CRIAÇÃO DE UMA FUNÇÃO QUE VALIDA O E-MAIL DO USUÁRIO//
+// CRIAÇÃO DE UMA FUNÇÃO QUE VALIDA O E-MAIL DO USUÁRIO  //
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+//------------------------------------------------------//
+// CRIAÇÃO DE EVENTO QUE ENVIA O E-MAIL, CASO ESTE ESTEJA VÁLIDO //
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    //validar e-mail
-    if (email.value === "") {
+    //USO DA FUNÇÃO (VALIDADEEMAIL) PARA VALIDAR O VALOR DO INPUT COMO EMAIL//
+    if (validateEmail(email.value) != '') {
+        form.submit()
+    } else {
         document.querySelector('input').style.borderColor = "red";
         document.getElementById('phrase3').style.display = 'block';  
         return
     }
-    // se o e-mail estiver preenchido corretamente, faça:
-    form.submit();
-    
+// CASO O INPUT CONTENHA OS CARACTERES REQUERIDOS PELA FUNÇÃO, O EVENTO ENCERRARÁ COM O SEU ENVIO.
+// CASO NÃO, AS DIVS ALERTARAM EM VERMELHO PARA QUE SE REESCREVA O INPUT.VALUE NOVAMENTE.
 })
+//------------------------------------ FIM DA EXECUÇÃO DO PROGRAMA --------------------------------------//
